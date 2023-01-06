55c7ca83ad183e82447ac464b2573bb738b5d2ae
Tyler Griffith <griffitht16@gmail.com>
2023-01-06 16:22:52 -0600

    Changed classes + Added Classes

--- src/main/java/org/example/Dinosaur.java
+++ src/main/java/org/example/Dinosaur.java
@@ -1,15 +1,19 @@
 package org.example;
 
 public class Dinosaur {
+    //(HAS A) or Instance Variables
     public static String name;
     public Weapon weapon;
 
+    //Constructor
     public Dinosaur(Weapon weapon) {
-        this.name = "Stegosaurus";
+        name = "Stegosaurus";
         this.weapon = weapon;
     }
+
+    //Methods or (CAN DO)
     public void Attack(Robot robot_one){
-        System.out.println(this.name + " attacks " + (Robot.name) + " with their " + this.weapon.name + " for " + this.weapon.power + " damage!");
+        System.out.println(name + " attacks " + (Robot.name) + " with their " + this.weapon.name + " for " + this.weapon.power + " damage!");
 
 
     }
--- src/main/java/org/example/Robot.java
+++ src/main/java/org/example/Robot.java
@@ -9,13 +9,13 @@ public class Robot {
 
     //Constructor
     public Robot(Weapon weapon) {
-        this.name = "Robonaut";
+        name = "Robonaut";
         this.weapon = weapon;
     }
 
     //Methods (CAN DO)
     public void Attack(Dinosaur dinosaur_one){
-        System.out.println(this.name + " attacks " + (Dinosaur.name) + " with their " + this.weapon.name + " for " + this.weapon.power + " damage!");
+        System.out.println(name + " attacks " + (Dinosaur.name) + " with their " + this.weapon.name + " for " + this.weapon.power + " damage!");
     }
 
 }
